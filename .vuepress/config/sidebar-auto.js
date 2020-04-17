2d3ab39cc0506253f0dc54a6bdc5a698c2de8d00
smw <shimingw@foxmail.com>
2020-04-17 17:59:19 +0800

    'update'

--- .vuepress/config/sidebar-auto.js
+++ .vuepress/config/sidebar-auto.js
@@ -6,7 +6,7 @@
 
   const JavaScript = [{"title":"ES6","collapsable":false,"children":[["01.ES6/01.ES6.md","ES6"]]},{"title":"React","collapsable":false,"children":[["02.React/01.React学习笔记.md","React学习笔记"],["02.React/02.Redux.md","Redux"]]},{"title":"Vue","collapsable":false,"children":[["03.Vue/01.vue笔记.md","vue笔记"],["03.Vue/02.vue-cli3.md","vue-cli3"],["03.Vue/03.VueTable学习笔记.md","VueTable学习笔记"]]},{"title":"其他","collapsable":false,"children":[["04.其他/01.深入浅出lodash.md","深入浅出lodash"],["04.其他/02.代码规范.md","代码规范"],["04.其他/03.函数式编程.md","函数式编程"],["04.其他/04.深入了解NodeList与HTMLCollection.md","深入了解NodeList与HTMLCollection"]]},{"title":"书籍学习笔记","collapsable":false,"children":[["05.书籍学习笔记/01.JavaScript设计模式—笔记.md","JavaScript设计模式—笔记"],["05.书籍学习笔记/02.JavaScript语言精粹—笔记.md","JavaScript语言精粹—笔记"]]}]
 
-  const React = [["01.常用API.md","常用API"],["02.实现redux.md","实现redux"],["03.实现react-redux.md","实现react-redux"]]
+  const React = [["01.常用API.md","常用API"],["02.实现redux.md","实现redux"],["03.实现react-redux.md","实现react-redux"],["04.实现ant-form-create.md","实现ant-form-create"],["05.实现递归组件Tree.md","实现递归组件Tree"],["06.实现react-router.md","实现react-router"]]
 
   const Vue = [["01.v-model原理.md","v-model原理"],["02.用vue实现表单组件.md","用vue实现表单组件"],["03.实现弹窗组件.md","实现弹窗组件"],["04.实现递归组件Tree.md","实现递归组件Tree"],["05.实现vue-route.md","实现vue-route"],["06.实现vuex.md","实现vuex"],["07.实现vue.md","实现vue"]]
 
@@ -18,7 +18,7 @@
 
   const 服务器搭建 = [["01.SSL的申请与部署.md","SSL的申请与部署"],["02.nginx.md","nginx"],["03.Linux.md","Linux"]]
 
-  const 源码学习笔记 = [{"title":"汇总","collapsable":false,"children":[["01.汇总/01.源码习笔记1.md","源码习笔记1"]]},{"title":"vue","collapsable":false,"children":[["02.vue/01.v-model原理.md","v-model原理"],["02.vue/02.用vue实现表单组件.md","用vue实现表单组件"],["02.vue/03.实现弹窗组件.md","实现弹窗组件"],["02.vue/04.实现递归组件Tree.md","实现递归组件Tree"],["02.vue/05.实现vue-route.md","实现vue-route"],["02.vue/06.实现vuex.md","实现vuex"],["02.vue/07.实现vue.md","实现vue"]]}]
+  const 源码学习笔记 = [["01.源码习笔记1.md","源码习笔记1"]]
 
   const 缓存 = [["01.service-worker.md","service-worker"],["02.pwa实践.md","pwa实践"]]
 
